3c6828635d730baac5f559d343a08157208fabc7
LLLLrb <lirongbin1000@163.com>
2019-09-11 16:52:23 +0800

    完成content-tab切换

--- js/HospitalRegistration.js
+++ js/HospitalRegistration.js
@@ -18,15 +18,28 @@ $.fn.search = function (){
 }
 
 
-
-$.fn.switchTab = function (){
-	let contentTab = $(this),
-		contentTab = $('.header-search-selected', headerSearch),
-		headerSearchSelectList = $('.header-search-selectList', headerSearch);
+/*
+*	@param String tabTitleName ——标签卡标题名称
+*	@param String focusClassName ——点击高亮CSS类名称
+*/
+$.fn.switchTab = function (tabTitleName, tabContainerName, focusClassName){
+	let tab = $(this),
+		tabTitleAs = $('.' + tabTitleName + ' > a', tab),
+		tabContainers = $('.' + tabTitleName + ' ~ .' + tabContainerName, tab);
+		console.log(tabContainers)
+		
+		tabTitleAs.on('click', function (){
+			tabTitleAs.removeClass(focusClassName)
+				.eq($(this).index()).addClass(focusClassName);
+			tabContainers.hide()
+				.eq($(this).index()).show();
+		});
 }
 
 
 
 $().ready(function (){
 	$('.header-search').search();
+	$('.content-tab').switchTab('content-tab-title', 'content-tab-container', 'focus');
+	$('.content-tab-title + .content-tab-container').switchTab('content-tab-subtitle', 'content-tab-container', 'focus');
 });
